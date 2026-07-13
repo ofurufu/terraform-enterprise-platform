05919b8f516bb07710d453e2dff46ee292288e40
ofurufu <ayodeleadewuyia+github@gmail.com>
2026-07-13 21:38:51 +0100

    feat: add NAT Gateway and private subnet internet access

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -89,6 +89,12 @@ resource "aws_route_table" "private" {
   }
 }
 
+
+resource "aws_route" "private_internet_access" {
+  route_table_id = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id = aws_nat_gateway.this.id
+}
 resource "aws_route_table_association" "private_1" {
   subnet_id      = aws_subnet.private_1.id
   route_table_id = aws_route_table.private.id
@@ -99,3 +105,26 @@ resource "aws_route_table_association" "private_2" {
   route_table_id = aws_route_table.private.id
 }
 
+resource "aws_eip" "nat" {
+  domain = "vpc"
+  tags = {
+    Name = "${var.project_name}-${var.environment}"
+    Environment = "var.environment"
+    ManagedBy = "Terraform"
+  }
+}
+
+resource "aws_nat_gateway" "this" {
+  allocation_id = aws_eip.nat.id
+  subnet_id = aws_subnet.public_1.id
+
+  depends_on = [ 
+    aws_internet_gateway.this 
+    ]
+  
+  tags = {
+    Name = "${var.project_name}-${var.environment}-nat-gateway"
+    Environment = "var.environment"
+    ManagedBy = "Terraform" 
+  }
+}
