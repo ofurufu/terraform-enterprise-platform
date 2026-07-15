1849fad63e271091fe141dafe9453d66ade20c64
ofurufu <ayodeleadewuyia+github@gmail.com>
2026-07-15 23:19:02 +0100

    feat: add production-ready EC2 module

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -24,3 +24,24 @@ module "security_group" {
 
   vpc_id = module.vpc.vpc_id
 }
+
+module "iam" {
+
+  source = "../../modules/iam"
+
+  project_name = var.project_name
+  environment  = var.environment
+
+}
+
+module "ec2" {
+  source = "../../modules/ec2"
+
+  project_name = var.project_name
+  environment  = var.environment
+
+  private_subnet_id     = module.vpc.private_subnet_ids[0]
+  ec2_security_group_id = module.security_group.ec2_security_group_id
+  instance_profile_name = module.iam.instance_profile_name
+  instance_type         = "t3.micro"
+}
